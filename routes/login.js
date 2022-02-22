aaa76f375ffd83bf80438179fb89d6bb9785fbaf
CollinsRollinsDev <collinsrollins07@gmail.com>
2022-02-22 22:29:19 +0100

    updated

--- routes/login.js
+++ routes/login.js
@@ -12,6 +12,12 @@ const cookieParser = require("cookie-parser");
 let app = express();
 app.use(cookieParser());
 let proceed;
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
 
 
 router.post("/", async (req, res) => {
