c6eae6e5bbf64f09f45afee3b318689522a68e21
CollinsRollinsDev <collinsrollins07@gmail.com>
2022-02-23 03:21:20 +0100

    update

--- routes/login.js
+++ routes/login.js
@@ -14,6 +14,7 @@ app.use(cookieParser());
 let proceed;
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   next();
